53e97277c012f22345efc8362d283e515f858ff9
binss <babywolf1@163.com>
2014-02-23 09:44:16 +0800

    Initial Commit

---
+++ MyGK/BINViewController.h
@@ -0,0 +1,13 @@
+//
+//  BINViewController.h
+//  MyGK
+//
+//  Created by bin on 14-2-23.
+//  Copyright (c) 2014年 bin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BINViewController : UIViewController
+
+@end
---
+++ MyGK/BINViewController.m
@@ -0,0 +1,29 @@
+//
+//  BINViewController.m
+//  MyGK
+//
+//  Created by bin on 14-2-23.
+//  Copyright (c) 2014年 bin. All rights reserved.
+//
+
+#import "BINViewController.h"
+
+@interface BINViewController ()
+
+@end
+
+@implementation BINViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
